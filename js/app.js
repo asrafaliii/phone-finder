6aba6916f321ae1b03b94d778d5c35757f3bf54d
asrafaliii <asraf.bookaand@gmail.com>
2022-03-02 18:36:40 +0600

    turnary operator

--- js/app.js
+++ js/app.js
@@ -3,12 +3,20 @@
 const allPhones = () => {
   // console.log('hello')
   const searchText = document.getElementById('search-box').value;
+
+
+  // const searchText = document.getElementById('search-box');
+  // const inputValue = searchText.value;
+  // if()
+
+
   // console.log(searchText);
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   // console.log(url);
   fetch(url)
   .then(res => res.json())
   .then(data => showPhones(data.data));
+
 };
 
 // show phones
@@ -20,19 +28,21 @@ const showPhones = (phones) => {
       
       for(const phone of first20Data){
         const div = document.createElement('div');
-        div.className = "col-lg-4 shadow p-1 mb-5 bg-body rounded"
+        div.className = "col-lg-4 "
         // console.log(phone.phone_name);
         div.innerHTML = `
-        <div class="text-center">
-           <img src="${phone.image}" class="card-img-top w-50" alt="...">
-      </div>
-      <div class="card-body">
-           <p class="card-text">${phone.phone_name}</p>
-           <div class="d-flex justify-content-between">
-             <h5 class="brand-title">${phone.brand}</h5>
-             <button onclick="details('${phone.slug}')" class="btn btn-info text-white">Details</button>
+        <div class="card shadow p-3 mb-5 bg-body rounded m-3" style="width: 18rem;">
+          <div class="text-center">
+            <img src="${phone.image}" class="card-img-top w-50" alt="...">
           </div>
-       </div>
+          <div class="card-body">
+            <p class="card-text">${phone.phone_name}</p>
+            <div class="d-flex justify-content-between">
+              <h5 class="brand-title">${phone.brand}</h5>
+              <button onclick="details('${phone.slug}')" class="btn btn-info text-white">Details</button>
+            </div>
+          </div>
+        </div>
         `
         parent.appendChild(div)
       }
@@ -46,34 +56,34 @@ const details = (id) => {
   .then(res => res.json())
   .then(data => showPhoneDetails(data.data));
 };
-
+// {/* <p>Release Date:  ${detail.releaseDate}</p> */}
 const showPhoneDetails = (detail) => {
   document.getElementById('details-container').innerHTML = `
-  <div class="row mb-5 mt-3">
+  <div class="row mb-5 mt-3 d-flex align-items-center shadow p-3 mb-5 bg-body rounded">
           <div class="col-md-4">
-            <div class="text-center">
+            <div class="text-center ">
               <img src="${detail.image}" class="card-img-top w-75" alt="...">
             </div>
           </div>
           <div class="col-md-8">
             <h3>  ${detail.name}</h3>
-            <p>Release Date:  ${detail.releaseDate}</p>
+           <p>Release Date:  ${detail.releaseDate ? detail.releaseDate: "Not Found"}</p>
             <h1>Main Features </h1>
 
             <h5 class="">Brand:<span class="">  ${detail.brand}</span></h5>
             <h5>Storage:  ${detail.mainFeatures.storage}</h5>
             <h5>Display Size:  ${detail.mainFeatures.displaySize}</h5>
             <h5>Memory:  ${detail.mainFeatures.memory}</h5>
-            <h5>Sensors:  ${detail.mainFeatures.sensors}</h5>
+            <h5>Sensors:  ${detail.mainFeatures.sensors ? detail.mainFeatures.sensors:"Not Found" }</h5>
             
             <h5>Others:</h5>
             <ul>
-            <li>WLAN: ${detail.others.WLAN}</li>
-            <li>Bluetooth: ${detail.others.Bluetooth}</li>
-            <li>GPS: ${detail.others.GPS}</li>
-            <li>NFC: ${detail.others.NFC}</li>
-            <li>Radio: ${detail.others.Radio}</li>
-            <li>USB: ${detail.others.USB}</li>
+            <li>WLAN: ${detail.others.WLAN ? detail.others.WLAN:"Not Found"}</li>
+            <li>Bluetooth: ${detail.others.Bluetooth ? detail.others.Bluetooth:"Not Found"}</li>
+            <li>GPS: ${detail.others.GPS ? detail.others.GPS:"Not Found"}</li>
+            <li>NFC: ${detail.others.NFC ? detail.others.NFC: "Not Found"}</li>
+            <li>Radio: ${detail.others.Radio ? detail.others.Radio: "Not Found"}</li>
+            <li>USB: ${detail.others.USB ? detail.others.USB: "Not Found"}</li>
             </ul>
           </div>
         </div>
